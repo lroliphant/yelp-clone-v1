c4e0564d8efec3687d877981756b2baf0523fb91
TheoLeanse <theoleanse@gmail.com>
2015-07-22 13:12:19 +0100

    generates devise within the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  root 'restaurants#index'
+
   resources :restaurants do
     resources :reviews
   end
@@ -9,6 +12,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
