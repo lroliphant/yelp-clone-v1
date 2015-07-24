f40f123872cf3bea21558fb3f5a77203b572bdc5
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-07-24 11:04:23 +0100

    Add user can only leave one review per restaurant

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,10 +5,27 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
+  # def create
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  #   @restaurant.reviews.create(review_params)
+  #   redirect_to restaurants_path
+  # end
+
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @restaurant.reviews.create(review_params)
-    redirect_to restaurants_path
+    @review = @restaurant.reviews.build_with_user(review_params, current_user)
+    if @review.save
+      redirect_to restaurants_path
+    else
+      if @review.errors[:user]
+        flash[:notice] = "You cannot review a restaurant twice"
+        redirect_to restaurants_path
+      else
+        render :new
+      end
+    end
+    # @restaurant.reviews.create(review_params)
+    # redirect_to restaurants_path
   end
 
   def review_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:facebook]
 
   has_many :restaurants
+  has_many :reviews
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
